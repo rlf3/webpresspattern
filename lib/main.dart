763a8fa618c281d20b06dc956df0bf32a33a8f6e
rlf3 <rlf3@hotmail.com>
2019-10-02 17:34:14 -0500

    login and database

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:webpresspattern/services/authentication.dart';
+import 'package:webpresspattern/pages/root_page.dart';
+
+void main() {
+  runApp(new MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+        title: 'Web press Login',
+        debugShowCheckedModeBanner: false,
+        theme: new ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: new RootPage(auth: new Auth()));
+  }
+}
